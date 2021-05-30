eab4401d773f786ca35f922980eedbe06b2e66f1
Gareth Jones <jones258@gmail.com>
2021-05-30 17:00:15 +1200

    wip: typescript tests

---
+++ variants/frontend-typescript/app/frontend/test/components/home/index.spec.tsx
@@ -0,0 +1,11 @@
+import Index from '../../../components/home';
+
+describe('Home', () => {
+  // it: supports typing first, middle, and last names
+  // it: shows an alert when submitting
+  describe('when the form is submitted', () => {
+    it('shows an alert with the persons name', () => {
+      render(<Index />);
+    });
+  });
+});
